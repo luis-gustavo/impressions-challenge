690a94677557ad42c639c915641f97ad99ac3569
Luis Gustavo Avelino <luis.gustavo3013@hotmail.com>
2021-04-02 13:18:06 -0300

    Add emprty xcode project

---
+++ ImpressionsChallenge/ImpressionsChallenge/Core/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  ImpressionsChallenge
+//
+//  Created by luis.gustavo.jacinto on 02/04/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
